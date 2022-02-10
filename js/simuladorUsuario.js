b81ee677225cfe0a3dd6e545c0b5d593a782926e
eugeGomez <eugequimica@gmail.com>
2022-02-09 21:38:16 -0300

    Desafio entregable eventos

--- js/simuladorUsuario.js
+++ js/simuladorUsuario.js
@@ -1,19 +1,17 @@
-// Pedido de Usuarios para la página, para que quede logueado quien pide. 
-function recepcion () {
-    alert("Bienvenido al Artesanal")
-    alert("Vamos a solicitarle los datos para que se convierta en un usuario")
-}
-recepcion()
-class Usuario {
-    constructor (usuario, contrasena, direccion) {
-        this.usuario = usuario;
-        this.contrasena = contrasena;
-        this.direccion = direccion;
-    }
-    recibir() {
-        alert("Tu direccion de entrega está cargada como " + this.direccion);
-    }
-}
-let usuario1 = new Usuario(prompt("Ingrese su usuario"), prompt("Ingrese su contraseña"), prompt("Ingrese lugar de recepción del pedido"))
-usuario1.recibir()
+let usuario1 = {usuario:document.getElementById("nombre"), direccion:document.getElementById("direccion"), celular:document.getElementById("celular") , contrasenia:document.getElementById("contra").value} 
 
+let clickear = document.getElementById("btningresar")
+      clickear.addEventListener("click", ingresar)
+function ingresar () {
+    let infodeusuaruio = document.getElementById("userinfo");
+    infodeusuaruio.innerHTML = `<p class="user">Usuario: ${usuario1.usuario.value}</p>
+                        <p class="user">Su dirección de entrega ingresada es: ${usuario1.direccion.value}</p>
+                        <p class="user">Su número de celular ingresada es: ${usuario1.celular.value}
+                        <p class="user">Su contraseña está verificada
+                        <hr>`
+}
+// Solo para sumar eventos. 
+let contrasenia = document.getElementById("contra")
+contrasenia.addEventListener('input', () => {
+      console.log(contrasenia.value)
+})
